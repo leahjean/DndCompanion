dd86bd270cdff0e1cdff0fef5b9199a4bde0c2bd
Kaucic <kaucic2@illinois.edu>
2017-11-11 18:18:18 -0600

    empty android studio project

---
+++ app/app/src/main/java/edu/uiuc/cs465/dndcompanion/StartScreen.java
@@ -0,0 +1,13 @@
+package edu.uiuc.cs465.dndcompanion;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class StartScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start_screen);
+    }
+}
